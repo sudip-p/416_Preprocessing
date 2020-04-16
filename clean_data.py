dac2f87204c7c086056a88fcefbb4cc4962c7149
Sudip Paul <sudip00paul@yahoo.com>
2020-04-16 03:07:12 -0400

    Converted county names in MD_json into actual names instead of FIPS code. Outputted MD_json with proper county names and canonical precinct names.

---
+++ clean_data.py
@@ -0,0 +1,6 @@
+import json
+
+# TODO: write this method to fix county names for MD
+def change_county_names():
+    x = 3
+
--- pp_usecases.py
+++ pp_usecases.py
@@ -7,18 +7,34 @@ parser = configparser.ConfigParser()
 parser.read('config.ini')
 config = parser['DEFAULT']
 
+
 # TODO: NEED TO FINISH WRITING THIS FUNCTION
 def rename_precincts(state):
+
+    # IF STATE IS MARYLAND, THEN PERFORM THE FOLLOWING OPERATIONS
     if state == 'MD':
-        # IF STATE IS MARYLAND, THEN
+
         precincts = []
         out = open(config['out_rel_dir'] + 'MD_precinct_names.txt', 'w')
         with open(config['MD_json']) as file:
             data = file.read()
             precinct_geoj = json.loads(data)
 
-            for f in precinct_geoj['features']:
-                out.write(f['properties']['NAME'] + "\n")
+            # Write the geojson to the outfile with canonical names added
+            md_counties = open(config['MD_dir'] + 'MD_counties_FIPS.txt', 'r')
+            Lines = md_counties.readlines()
+            county_dict = {}
+
+            for line in Lines:
+                compartments = line.split(',')
+                compartments[1] = compartments[1].replace(' ', '')
+                county_dict[compartments[1][0:5]] = compartments[0]
+
+            for item in precinct_geoj['features']:
+                item['properties']['COUNTY'] = county_dict[item['properties']['COUNTY']]
+                item['properties']['CANON_NAME'] = item['properties']['COUNTY'] + "_" + item['properties']['NAME']
+
+            out.write(json.dumps(precinct_geoj))
 
             file.close()
             out.close()
